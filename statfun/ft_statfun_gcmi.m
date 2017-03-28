769ebb329dc8f863b53efd53be65ae6715f8760e
Robin Ince <robince@gmail.com>
2017-03-28 13:05:44 +0200

    gcmi statfun with 3d multivariate vector interface

--- statfun/ft_statfun_gcmi.m
+++ statfun/ft_statfun_gcmi.m
@@ -7,167 +7,142 @@ function [stat, cfg, dat] = ft_statfun_gcmi(cfg, dat, design)
 %  cfg.preconditionflag = 0 (default), or 1, performs Gaussian copula transform
 %    Preconditioning is computationally efficient, because for given data
 %    it needs to be done only once.
+% cfg.gcmi.method = ['cc', 'cd_model' 'cd_mixture']
+%    Type of calculation
+% cfg.gcmi.complex = ['abs' 'real' 'imag' 'complex' 'angle' ]
+%    How to treat complex data
+% cfg.gcmi.tra = matrix which specifies multivariate structure
 
-cfg.mi               = ft_getopt(cfg, 'mi', []);
-cfg.preconditionflag = ft_getopt(cfg, 'preconditionflag', false);
-
+if ~strcmp(cfg.precondition,'before')
+  error('ft_statfun_gcmi: must use precondition=before')
+end
+if ~isfield(cfg,'preconditionflag')
+  error('ft_statfun_gcmi: must specify preconditionflag')
+end
+cfg.gcmi             = ft_getopt(cfg, 'gcmi', []);
+if ~isfield(cfg.gcmi, 'method')
+  error('ft_statfun_gcmi: must specify gcmi metho')
+end
+cfg.gcmi.complex     = ft_getopt(cfg.gcmi, 'complex', false);
 % get this one as a local variable only
-tra                  = ft_getopt(cfg, 'tra', []);
+tra                  = ft_getopt(cfg.gcmi, 'tra', []);
 
 % check the validity of the design
-if ~all(ismember(design(cfg.ivar,:), 1:max(design(cfg.ivar,:)))),
-  error('the design vector is ill-specified');
+if strcmp(cfg.gcmi.method,'cd_model') || strcmp(cfg.gcmi.method,'cd_mixture')
+  % discrete valued ivar, 1-based
+  Y = design(cfg.ivar, :)';
+  % number of classes
+  Ym = max(Y);
+  if ~all(ismember(Y, 1:Ym));
+    error('ft_statfun_gcmi: the design vector is ill-specified');
+  end
+elseif strcmp(cfg.gcmi.method,'cc')
+  % continuous values
+  error('ft_statfun_gcmi: cc not implemented yet')
+else
+  error(sprintf('ft_statfun_gcmi: unknown method \"%s\"', cfg.gcmi.method));
 end
-N      = max(design(cfg.ivar,:));
-Y      = design(cfg.ivar, :)' - 1; % convention of the lower level code
 
-if cfg.preconditionflag,
-  % deal with planar gradient MEG data
-  if isfield(cfg, 'channel') && ft_senstype(cfg.channel, 'meg_planar')
-    cfg.mi.combineplanar = ft_getopt(cfg.mi, 'combineplanar', 'no');
-    
-    if istrue(cfg.mi.combineplanar)
-      % find the combination of horizontal and vertical channels that should be combined
-      planar      = ft_senslabel(ft_senstype(cfg.channel), 'output', 'planarcombined');
-      
-      [~, sel_dH] = match_str(planar(:,1), cfg.channel);  % indices of the horizontal channels
-      [~, sel_dV] = match_str(planar(:,2), cfg.channel);  % indices of the vertical   channels
-      
-      % find the other channels that are present in the data
-      sel_other = setdiff(1:length(cfg.channel), [sel_dH(:)' sel_dV(:)']);
-      lab_other = cfg.channel(sel_other);
-      
-      
-      if length(sel_dH)~=length(sel_dV)
-        error('not all planar channel combinations are complete')
-      end
-      
-      % FIXME check for completeness, and that only planar channels are
-      % in input
-      
-      % define the channel names after combining the planar combinations
-      % they should be sorted according to the order of the planar channels in the data
-      [~, sel_planar] = match_str(cfg.channel(sel_dH),planar(:,1));
-      lab_comb        = planar(sel_planar,end);
-      
-      tok     = tokenize(cfg.dimord, '_');
-      chandim = find(strcmp(tok, 'chan')); % FIXME works only with chandim==1
-      
-      indx_in  = reshape((1:size(dat,1)), cfg.dim);
-      newdim   = cfg.dim;
-      newdim(chandim) = numel(lab_comb)+numel(lab_other);
-      indx_out = reshape((1:prod(newdim)), newdim);
-      
-      % create a prod(cfg.dim) x prod(newdim) sparse matrix that instructs how to combine rows of dat
-      x = zeros(0,1);
-      y = zeros(0,1);
-      for k = 1:numel(lab_comb)
-        x = cat(1,x,reshape(cat(1,indx_in(sel_dH(k),:),indx_in(sel_dV(k),:)),[],1));
-        y = cat(1,y,reshape(cat(1,indx_out(k,:),indx_out(k,:)),[],1));
-      end
-      for k = numel(lab_comb)+(1:numel(lab_other))
-        x = cat(1,x,reshape(indx_in(sel_other(k-numel(lab_comb)),:),[],1));
-        y = cat(1,y,reshape(indx_out(k,:),[],1));
-      end
-      tra = sparse(x,y,ones(numel(x,1)));
-      
-    end
+datT = dat.';
+if cfg.preconditionflag
+  
+  if ischar(cfg.gcmi.complex) && isreal(dat)
+    error('ft_statfun_gcmi: data not complex')
+  end
+  if ~ischar(cfg.gcmi.complex) && ~isreal(dat)
+    error('ft_statfun_gcmi: complex option not specified')
   end
   
-  if istrue(cfg.mi.addgradient)
-    if isempty(tra)
-      tra = speye(size(dat,1));
-    end
-    
-    tok     = tokenize(cfg.dimord, '_');
-    timedim = find(strcmp(tok, 'time')); % FIXME works only with chandim==1
-    if timedim~=2,
-      error('cfg.mi.addgradient is not supported for the given data');
-    end
-    
-    fprintf('computing the temporal derivative\n');
-    dat_gradient = zeros(size(dat));
-    for k = 1:size(dat,2)
-      dat_gradient(:,k) = reshape(ft_preproc_derivative(reshape(dat(:,k),cfg.dim)),[],1);
-    end
-    dat = cat(1,dat,dat_gradient);
-    clear dat_gradient;
-    tra = cat(1,tra,tra);
-  end
-    
-  if istrue(cfg.mi.combineplanar)
-    % these can now be updated (i.e. only after the addgradient part)
-    cfg.dim     = newdim;
-    cfg.channel = [lab_comb(:);lab_other(:)];
-  end
-  cfg.tra     = tra; % give it back to the cfg, for re-use
 
-  
+  if cfg.gcmi.complex
+    tra = speye(size(dat,1));
+    switch cfg.gcmi.complex
+      case 'complex'
+        % tease apart the real/imag parts, treat as 2D-variable
+        datT = cat(2, real(datT), imag(datT));
+        tra = cat(1, tra, tra);
+      case 'abs'
+        % take the amplitude
+        datT = abs(datT);
+      case 'angle'
+        % tease apart the real/imag parts, after amplitude normalization,
+        datT = datT ./ abs(datT);
+        datT = cat(2, real(datT), imag(datT));
+        tra = cat(1, tra, tra);
+      case 'real'
+        % just real part
+        datT = real(datT);
+      case 'imag'
+        % just imag part
+        datT = imag(datT);
+      otherwise
+        error(sprintf('ft_statfun_gcmi: unsupported value \"%s\" for gcmi.complex',complex));
+    end
+  else
+    % no multivariate or other transforms
+    tra = speye(size(dat,1));
+  end
   fprintf('performing the copula-transform\n');
   % FIXME here we should deal with NaNs in the data, note that these can be different across rows (althought that is rare), which prevents the possibility of doing the transform in a single call.
-  dat  = copnorm(dat')';
-  stat = [];
+  % TODO - unconditional copnorm for cd_mixture?
+  dat  = copnorm(datT)';
+  % save tra for permutation non-precondition runs
+  cfg.gcmi.tra = tra;
+  
+  % build multivariate response once
+  % Q: will this work with bootstrap?
+  Nvar     = full(sum(tra,1));
+  % number of multivariate dimensionalities
+  uNvar    = unique(Nvar);
+  datT = dat';
+  % create multivariate data matrix from tra
+  % loop over dimensionality k
+  datcel = cell(numel(uNvar),1);
+  uNvarN = zeros(numel(uNvar),1);
+  selcol = cell(numel(uNvar),1);
+  for k = 1:numel(uNvar)
+    % multivariate output
+    sel_col  = Nvar==uNvar(k);
+    % input index
+    sel_row  = full(sum(tra(:,sel_col),2)>0);
+    
+    sel_dat = datT(:,sel_row);
+    sel_tra = tra(sel_row,sel_col);
+    [ix, iy] = find(sel_tra);
+    % multivariate data matrix
+    datmc = zeros(size(sel_dat,1), length(sel_col), k);
+    for vi1=1:uNvar(k)
+      % all voxels for this dimension
+      indx1 = ix(vi1:uNvar(k):numel(ix));
+      datmc(:,:,vi1) = sel_dat(:,indx1);
+    end
+    datcel{k} = datmc(:,:)';
+    uNvarN(k) = size(datcel{k},1);
+    selcol{k} = sel_col;
+  end
+  cfg.gcmi.uNvar = uNvar;
+  cfg.gcmi.uNvarN = uNvarN;
+  cfg.gcmi.selcol = selcol;
+  dat = cell2mat(datcel);
 end
 
-if isempty(tra)
-  tra = speye(size(dat,1));
+stat = [];
+
+% for each size of multivariate calculation unpack the required data
+uNvar = cfg.gcmi.uNvar;
+uNvarN = cfg.gcmi.uNvarN;
+selcol = cfg.gcmi.selcol;
+
+startidx = 1;
+datT = dat';
+Ntrl = size(dat,2);
+for k=1:numel(uNvar)
+  endidx = startidx + uNvarN(k) - 1;
+  datmc = datT(:,startidx:endidx);
+  datmc = reshape(datmc, [Ntrl uNvarN(k)./uNvar(k) uNvar(k)]);
+  mi = mi_model_gd_vec(datmc, Y, Ym, true, true);
+  stat.stat(selcol{k},1) = mi;
 end
 
-% convert the design to a boolean matrix
-Y = indexed2boolean(Y);
-
-% remove the class-specific mean only once (is not done anymore in mi_gd2
-[dat, class_means] = centercolumns(dat,Y);
 
 
-Nvar     = full(sum(tra,1));
-uNvar    = unique(Nvar);
-for k = 1:numel(uNvar)
-  sel_col  = Nvar==uNvar(k);
-  sel_row  = full(sum(tra(:,sel_col),2)>0);
-
-  mi = mi_gd_vectorized(dat(sel_row,:)',Y,tra(sel_row,sel_col), true, true, class_means(sel_row,:)');
-  stat.stat(sel_col,1) = mi;
-end
-
-% mi = zeros(size(tra,2),1);
-% for k = 1:size(tra,2)
-%   mi(k) = mi_gd2(dat(tra(:,k)>0,:)',Y, N, true, true, class_means(tra(:,k)>0,:)'); %JM's version that works with persistent variables, to speed up some computations
-% end
-% stat.stat = mi;
-
-if nargout>2,
-  % add the class-specific means back to the data
-  dat = addclassmeans(dat,Y,class_means);
-end
-
-function [dat, class_means] = centercolumns(dat, design)
-
-%udesign     = unique(design);
-%class_means = zeros(size(dat,1),numel(udesign));
-class_means = zeros(size(dat,1),size(design,2));
-%for k = 1:numel(udesign)
-for k = 1:size(design,2)
-  %sel = design==udesign(k);
-  sel = design(:,k);
-  class_means(:,k) = mean(dat(:,sel),2);
-  dat(:,sel) = bsxfun(@minus,dat(:,sel),class_means(:,k));
-end
-
-function dat = addclassmeans(dat,design,class_means)
-
-%udesign = unique(design);
-%for k = 1:numel(udesign)
-for k = 1:size(design,2)
-  %sel = design==udesign(k);
-  sel = design(:,k);
-  dat(:,sel) = bsxfun(@plus,dat(:,sel),class_means(:,k));
-end
-
-function Y = indexed2boolean(X)
-
-uX = unique(X);
-Y  = false(numel(X),numel(uX));
-for k = 1:size(Y,2)
-  Y(X==uX(k),k) = true;
-end
